63a486d8e763dc7d8c28cdeead5fca53ec8c2f29
ChanYeongShin <cyshin@yonsei.ac.kr>
2021-03-28 21:26:03 +0900

    [ADDED] wasserstein loss for discriminator stable learning

--- models/warping_cloth_model.py
+++ models/warping_cloth_model.py
@@ -6,11 +6,12 @@ from . import networks
 from util.gramMatrix import StyleLoss
 import torchvision
 import torch.nn.functional as F
+from util.wassestein_loss import calc_gradient_penalty
 
 
-class GMMModel(BaseModel):
+class WarpingClothModel(BaseModel):
     def name(self):
-        return 'GMMModel'
+        return 'WarpingClothModel'
 
     def initialize(self, opt):
         BaseModel.initialize(self, opt)
@@ -18,20 +19,20 @@ class GMMModel(BaseModel):
         # specify the training losses you want to print out. The program will call base_model.get_current_losses
         self.loss_names = ['L1', 'gan', 'G', 'D']
         # specify the images G_A'you want to save/display. The program will call base_model.get_current_visuals
-        visual_names_A = ['real_image_mask', 'input_mask', 'warped_cloth']
+        visual_names_A = ['real_image_mask', 'cloth_mask', 'warped_cloth']
 
         self.visual_names = visual_names_A
         # specify the models you want to save to the disk. The program will call base_model.save_networks and base_model.load_networks
         if self.isTrain:
-            self.model_names = ['GMM']
+            self.model_names = ['G', 'D']
         else:  # during test time, only load Gs
-            self.model_names = ['GMM']
+            self.model_names = ['G']
 
         # load/define networks
         self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
                                         not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
         use_sigmoid = opt.no_lsgan
-        self.netD = networks.define_D(opt.input_nc, opt.ndf, opt.netD,
+        self.netD = networks.define_D(3, opt.ndf, opt.netD,
                                             opt.n_layers_D, opt.norm, use_sigmoid, opt.init_type, opt.init_gain, self.gpu_ids)
 
         if self.isTrain:
@@ -62,28 +63,31 @@ class GMMModel(BaseModel):
         self.warped_cloth = self.netG(torch.cat([self.real_image_mask, self.cloth_mask], dim=1))
 
     def backward_G(self):
-        self.loss_L1 = self.criterionL1(self.warped_cloth, self.image_mask)
+        self.loss_L1 = 10 * self.criterionL1(self.warped_cloth, self.image_mask)
         self.loss_gan = self.criterionGAN(self.netD(self.warped_cloth), True)
         self.loss_G = self.loss_L1 + self.loss_gan
-        self.loss_G.backward()
+        self.loss_G.backward(retain_graph=True)
 
     def backward_D(self):
-        pred_real = self.netD(self.image_mask)
-        self.loss_D_1 = self.criterionGAN(pred_real, True)
-
-        pred_fake = self.netD(self.warped_cloth.detach())
-        self.loss_D_2 = self.criterionGAN(pred_fake, False)
-        self.loss_D = self.loss_D_1 + self.loss_D_2
-        self.loss_D.backward()
+        # pred_real = self.netD(self.image_mask)
+        # self.loss_D_1 = self.criterionGAN(pred_real, True)
+        #
+        # pred_fake = self.netD(self.warped_cloth.detach())
+        # self.loss_D_2 = self.criterionGAN(pred_fake, False)
+        # self.loss_D = (self.loss_D_1 + self.loss_D_2) * 0.5
+        grad_penalty_A = calc_gradient_penalty(self.netD, self.warped_cloth, self.image_mask)
+        self.loss_D = torch.mean(self.netD(self.warped_cloth)) - torch.mean(self.netD(self.image_mask)) + grad_penalty_A
+        self.loss_D.backward(retain_graph=True)
 
     def optimize_parameters(self):
         # forward
         self.forward()
+        self.set_requires_grad([self.netD], False)
         self.optimizer_G.zero_grad()
         self.backward_G()
         self.optimizer_G.step()
 
-        self.set_requires_grad([self.netG], False)
+        self.set_requires_grad([self.netD], True)
         self.optimizer_D.zero_grad()
         self.backward_D()
         self.optimizer_D.step()
